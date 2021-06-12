ba1e85ace6fec569d13aa8617f3916430331e2a8
baalajimaestro <me@baalajimaestro.me>
2021-06-12 17:34:51 +0530

    Set PATH on bashrc
    Codespaces is resetting PATH to default, so fix it
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- setup-container.sh
+++ setup-container.sh
@@ -19,6 +19,21 @@ git config --global user.signingkey 35EA585CF08135747CE5DDB4F93C394FE9BBAFD5
 git config --global core.editor nano
 git config --global push.gpgSign if-asked
 git config --global init.defaultBranch master
+
+# TTY Specific Customisation
 echo 'export GPG_TTY=$(tty)' >> /workspaces/.bashrc
 echo 'export PS1="[\u@\h \W]\\$ "' >> /workspaces/.bashrc
+
+# Codespaces happens to reset the PATH, so set it back
+echo 'export RUSTUP_HOME=/usr/local/rustup' >> /workspaces/.bashrc
+echo 'export CARGO_HOME=/usr/local/cargo' >> /workspaces/.bashrc
+echo 'export RUST_VERSION=nightly' >> /workspaces/.bashrc
+echo 'export ANDROID_SDK_ROOT="/opt/android-sdk"' >> /workspaces/.bashrc
+echo 'export ANDROID_HOME="/opt/android-sdk"' >> /workspaces/.bashrc
+echo 'export CMDLINE_VERSION="4.0"' >> /workspaces/.bashrc
+echo 'export SDK_TOOLS_VERSION="7302050"' >> /workspaces/.bashrc
+echo 'export BUILD_TOOLS_VERSION="30.0.2"' >> /workspaces/.bashrc
+echo 'export LANG="C.UTF-8"' >> /workspaces/.bashrc
+echo 'export PATH="$PATH:/usr/local/cargo/bin:${ANDROID_SDK_ROOT}/cmdline-tools/${CMDLINE_VERSION}/bin:${ANDROID_SDK_ROOT}/platform-tools"' >> /workspaces/.bashrc
+
 rm -rf /tmp/keys
