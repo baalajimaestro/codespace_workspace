e75195a1eda9b4c32cb2a2ec3812641b4375e145
baalajimaestro <me@baalajimaestro.me>
2021-07-04 13:25:13 +0530

    Avoid prune for now
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I792834046439e663a055fcbf49fb43bf4a490b2b

--- attach-container.sh
+++ attach-container.sh
@@ -2,12 +2,3 @@
 
 sudo /usr/sbin/sshd
 sudo chgrp docker /var/run/docker.sock
-
-if [ ! -f "/workspaces/.dockerprune" ]
-then
-    echo "Performing initial prune, please wait!"
-    sudo docker system prune -a -f
-    touch /workspaces/.dockerprune
-else
-    echo "Reconnecting to container, so avoiding prune!"
-fi
